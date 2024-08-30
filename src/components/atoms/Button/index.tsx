99ef5571222cec2049d164bb34f7cddc1d1163f4
HarukiInoue1 <inoueh@tanapism.co.jp>
2024-08-30 18:46:24 +0900

    hooks test

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,14 +1,13 @@
 import { useRouter } from "next/router";
 import { FC } from "react";
-import styles from "./index.module.scss"
+import styles from "./index.module.scss";
 
-
-
-export const Button: FC<{buttonText:string}> = ({buttonText}) => {
-
+export const Button: FC<{ buttonText: string; onClick: () => void }> = ({
+  buttonText, onClick
+}) => {
   return (
     <>
-      <button className={styles.button1}>{buttonText}</button>
+      <button className={styles.button1} onClick={onClick}>{buttonText}</button>
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,38 @@
 import { NextPage } from "next";
-import styles from "./index.module.scss"
+import styles from "./index.module.scss";
 import { TopTitle } from "@/components/atoms/TopTitle";
 import { Button } from "@/components/atoms/Button";
+import { Counter } from "@/components/molecules/Counter";
+import { useEffect, useState } from "react";
+import cn from "classnames";
+import { useShowCounter } from "@/hooks/useShowCounter";
 
 const Home: NextPage = () => {
+  const [changeBgc, setChangeBgc] = useState(false);
+
+  const [showCounter, handleButtonClick] = useShowCounter();
+
+  useEffect(() => {
+    setChangeBgc((prev) => !prev);
+  }, [showCounter]);
+
   return (
     <>
       <TopTitle />
-      <Button buttonText="ログイン" />
-      <div>test</div>
+      <Button
+        buttonText="カウントコンポーネントの呼び出し"
+        onClick={handleButtonClick}
+      />
+      <div
+        className={cn({
+          [styles.test]: changeBgc,
+        })}
+      >
+        test
+      </div>
+      {showCounter && <Counter />}
     </>
   );
-}
+};
 
 export default Home;
